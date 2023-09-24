0e4027790522d45a583b265c88a7490a25677cca
lenard <energeticbowlofrice@gmail.com>
2023-09-24 19:56:30 +0800

    Added an endpoint for delete requests and connected a sqlite database

--- app.py
+++ app.py
@@ -1,40 +1,66 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-languages = [{"name": "Python"}, {"name": "Javascript"}, {"name": "C"}]
-
-def create_app():
-  app = Flask(__name__)
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
   
-  @app.route("/lang", methods=["GET"])
-  def getAll():
-    return jsonify({"languages": languages})
-
-  @app.route("/lang/<string:name>")
-  def getOne(name):
-    language = []
-    
-    for lang in languages:
-      if lang["name"] == name:
-        language.append(lang)
-    
-    return jsonify({"language": language[0]})
+db = SQLAlchemy(app)
   
-  @app.route("/lang", methods=["POST"]) 
-  def addOne():
-    languages.append({"name": request.json["name"]})
-    
-    return jsonify({"languages": languages})
-    
-  @app.route("/lang/<string:name>", methods=["PUT"])
-  def updateOne(name):
-    language = []
-    
-    for lang in languages:
-      if lang["name"] == name:
-        language.append(lang)
-    
-    language[0]["name"] = request.json["name"]
-    
-    return jsonify({"language": language[0]})
+class Language(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, unique=True, nullable=False)
+  
+@app.route("/lang", methods=["GET"])
+def getAll():
+  languages = []
+  
+  for language in Language.query.all():
+    languages.append({"id": language.id, "name": language.name})
+  
+  return jsonify({"languages": languages})
 
-  return app
+@app.route("/lang/<int:id>")
+def getOne(id):
+  language = Language.query.filter_by(id=id).first()
+  
+  return jsonify({"language": {"id": language.id, "name": language.name}})
+
+@app.route("/lang", methods=["POST"]) 
+def addOne():
+  new_language = Language(name=request.json["name"])
+  
+  db.session.add(new_language)
+  db.session.commit()
+  
+  languages = []
+  for language in Language.query.all():
+    languages.append({"id": language.id, "name": language.name})
+    
+  return jsonify({"languages": languages})
+    
+@app.route("/lang/<int:id>", methods=["PUT"])
+def updateOne(id):
+  language = Language.query.filter_by(id=id).first()
+  
+  language.name = request.json["name"]
+  db.session.commit()
+  
+  languages = []
+  for language in Language.query.all():
+    languages.append({"id": language.id, "name": language.name})
+  
+  return jsonify({"languages": languages})
+  
+@app.route("/lang/<int:id>", methods=["DELETE"])
+def deleteOne(id):
+  language = Language.query.filter_by(id=id).first()
+  
+  db.session.delete(language)
+  db.session.commit()
+  
+  languages = []
+  for lang in Language.query.all():
+    languages.append({"id": lang.id, "name": lang.name})
+    
+  return jsonify({"languages": languages})
+  
